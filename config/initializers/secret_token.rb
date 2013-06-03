9d0625d5bd3c8ae2d0a64fc78f7a406c50cc9fce
Aristide Niyungeko <niyungeko@gmail.com>
2013-06-03 05:25:30 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VAGABONDS::Application.config.secret_token = 'a1fcf4667580aa92e2dff2cae0d243c8b84f2708db1570bcd13f9d3b2c947757a7b7513d852df82a5224671a76e6134402e1d54e321e62464e3e6d9bc2f37e24'
