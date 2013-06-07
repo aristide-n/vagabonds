a36a2cf729f8b59dc6941572c5f32af82841aa29
Shama Hoque <shama.hoque@gmail.com>
2013-06-06 20:29:48 -0700

    Form input to itinerary page

--- UI/js/jquery.tagsinput.js
+++ UI/js/jquery.tagsinput.js
@@ -177,10 +177,10 @@
 	$.fn.tagsInput = function(options) { 
     var settings = jQuery.extend({
       interactive:true,
-      defaultText:'add a tag',
+      defaultText:'add activities',
       minChars:0,
-      width:'300px',
-      height:'100px',
+      width:'400px',
+      height:'30px',
       autocomplete: {selectFirst: false },
       'hide':true,
       'delimiter':',',
