2087694f720eae953f257946b997de68ea86db5d
Aristide Niyungeko <niyungeko@gmail.com>
2013-07-01 11:17:29 -0700

    make duration field accessible

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,5 @@
 class Place < ActiveRecord::Base
-  attr_accessible :address, :address_lat, :address_lng, :name, :permanent_id_num, :phone_number, :price_level, :rating, :reference, :url, :website, :review_summary
+  attr_accessible :address, :address_lat, :address_lng, :name, :permanent_id_num, :phone_number, :price_level, :rating, :reference, :url, :website, :review_summary, :duration
   validates_uniqueness_of :permanent_id_num
   has_many :periods
   has_many :reviews
