d896cd17deb8418f685297f6156695072af12401
Aristide Niyungeko <niyungeko@gmail.com>
2013-07-10 22:28:29 -0700

    Make photo reference type text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130711041907) do
+ActiveRecord::Schema.define(:version => 20130711052536) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(:version => 20130711041907) do
   end
 
   create_table "photos", :force => true do |t|
-    t.string   "reference_id"
+    t.text     "reference_id"
     t.integer  "height"
     t.integer  "width"
     t.integer  "place_id"
